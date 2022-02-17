1d09ac5ad8395a1885f3eec4c6d6eb3b1a8f7896
it21296796 <99311998+it21296796@users.noreply.github.com>
2022-02-17 13:14:10 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -12,18 +12,67 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+using namespace std ;
 
 long Factorial(int no);
 long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
+  cout << "Enter a value for n ";
+  cin >> n;
+  cout << "Enter a value for r ";
+  cin >> r;
+  cout << "nCr = ";
+  cout << nCr(n,r);
+  cout << endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+  int x ;
+  long fact = 1  ;
+
+  for( x = no ; x >= 1 ; x-- )
+    {
+      fact *= x ;
+    }
+
+  return fact ;
+}
+
+long nCr(int n, int r)
+{
+  long factn , factr , factnr ;
+  float final ;
+  
+  factn =  Factorial(n);
+  factr =  Factorial(r);
+  factnr =  Factorial(n-r);
+
+  final = factn / (factr * factnr) ;
+
+  return final ;
+  
+}
+  
+  /*int factn = 1 ;
+  
+  for(x = n ; x >= 1 ; x--)
+    {
+      factn *= factn * x  ;
+    }
+
+  for( x = r ; x >= 1 ; x--  )
+    {
+      factr *= factr * x ;
+    }
+
+  for( x = (n-r) ; x >= 1 ; x++ )
+    {
+      factn-r *= factn-r * x ;
+    }*/
+
+  
+
