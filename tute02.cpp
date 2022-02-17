cb439c736551f94474a8ac47d3a6e1bbb6811b03
it21296796 <99311998+it21296796@users.noreply.github.com>
2022-02-17 11:55:58 +0000

    completed

--- tute02.cpp
+++ tute02.cpp
@@ -12,17 +12,22 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+#include <iostream>
+using namespace std ;
+
 int main()
 {
    double salary, netSalary;
    int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
+  
+   cout<<"Enter employee type : " ;
+   cin >>etype ;
+  
+   cout<<"Enter salary : " ;
+   cin>>salary ;
+  
+   cout<<"Enter OTHrs : " ;
+   cin>>otHrs ;
    
    switch (etype) {
       case 1 :
@@ -38,7 +43,8 @@ int main()
 
 
    netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+  
+   cout<<"Net Salary is "<<netSalary ;
   
    return 0;
 }
